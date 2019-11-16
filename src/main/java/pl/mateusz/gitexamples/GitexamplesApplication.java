b7e6f72ce93467e6eec6ab90268aeed260496a56
Mati <kargo90@wp.pl>
2019-11-16 10:00:55 +0100

    [init] -initial project commit

---
+++ src/main/java/pl/mateusz/gitexamples/GitexamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.gitexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitexamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateusz/gitexamples/GitexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.gitexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
